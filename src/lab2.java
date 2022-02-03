354872f195f7061cde5c39c77859279b06ea80f7
t00201634 <Bryan.OShea@students.ittralee.ie>
2022-02-03 15:45:36 +0000

    finish lab2

--- src/lab2.java
+++ src/lab2.java
@@ -1,33 +1,39 @@
 public class lab2 {
-    public static String Encrypter(String plaintext, int numColumns) {
-        plaintext = plaintext.replaceAll("\\s+","").toUpperCase();
+    public static class Encrypter {
+        public static String encryptRowColumn(String plaintext, int numColumns) {
+            plaintext = plaintext.replaceAll("\\s+", "").toUpperCase();
 
-        int numRows = plaintext.length() / numColumns;
-        StringBuilder ciphertext = new StringBuilder();
-        int addXes = 0;
+            int numRows = plaintext.length() / numColumns;
+            StringBuilder ciphertext = new StringBuilder();
+            int addXes = 0;
 
-        if (plaintext.length() % numColumns != 0) {
-            addXes = numColumns - (plaintext.length() % numColumns);
-            numRows++;
-        }
-
-        plaintext += "X".repeat(addXes);
-
-        String[] text = plaintext.split("");
-
-        for (int col = 0; col <= numColumns - 1; col++) {
-            int index = col;
-            for (int row = 0; row <= numRows - 1; row++) {
-                ciphertext.append(text[index]);
-                index += numColumns;
+            if (plaintext.length() % numColumns != 0) {
+                addXes = numColumns - (plaintext.length() % numColumns);
+                numRows++;
             }
+
+            plaintext += "X".repeat(addXes);
+
+            String[] text = plaintext.split("");
+
+            for (int col = 0; col <= numColumns - 1; col++) {
+                int index = col;
+                for (int row = 0; row <= numRows - 1; row++) {
+                    ciphertext.append(text[index]);
+                    index += numColumns;
+                }
+            }
+
+            return ciphertext.toString();
         }
 
-        return ciphertext.toString();
+        public static String decryptRowColumn(String cipherText, int numColumns) {
+
+        }
     }
 
     public static void main(String[] args) {
-        String plaintext = "This is a secret message";
-        System.out.println(Encrypter(plaintext, 5));
+        String plaintext = "secret message";
+        System.out.println(Encrypter.encryptRowColumn(plaintext, 4));
     }
 }
